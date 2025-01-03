bf21aa845ad9f3d5379bc1af4ea30b4dbd2018bf
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-01-03 23:28:36 +0530

    bohot mehnat and kaam chalau going on

---
+++ src/features/CartSlice.jsx
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { value: 0 }
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {},
+})
+   
+  
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { store } from './app/store.jsx';
+import { store } from './app/store.js';
 import { Provider } from "react-redux";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
