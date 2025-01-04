a4357b4f9b092d5f92390da2d526b7bc6a10455a
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-01-04 23:56:25 +0530

    working with reducers now

--- src/Components/Product_Card.jsx
+++ src/Components/Product_Card.jsx
@@ -10,16 +10,19 @@ import {
   MDBRow,
   MDBCol,
 } from "mdb-react-ui-kit";
+import { useSelector } from "react-redux";
+
 
 
 const ProductCard = () => {
+    const items = useSelector((item) => state.)
 
 
   return (
     <div className="m-2">
       <MDBContainer>
         <MDBRow className="mb-3">
-         
+         {/* hehehe */}
             <MDBCol  size="4">
               <MDBCard>
                 <MDBCardImage src="https://www.91-img.com/gallery_images_uploads/3/d/3df5ca6a9b470f715b085991144a5b76e70da975.JPG?tr=h-550,w-0,c-at_max" position="top" alt="..." />
