be8fcc4d08c973d4ab4d865a431e52c6d75ed66c
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-01-06 01:37:50 +0530

    the cart is being read upon adding to cart but is not showing in website yet

--- src/Components/CartPage.jsx
+++ src/Components/CartPage.jsx
@@ -16,9 +16,14 @@ import {
     MDBTypography,
     } from "mdb-react-ui-kit";
     import React from "react";
+    import { useSelector } from "react-redux";
     
     export default function PaymentMethods() {
-    return (
+        const  {cart, totalQuantity, totalPrice } = useSelector((state) => state.allCart);
+   
+        return (
+
+        
     <section className="h-100 gradient-custom">
       <MDBContainer className="py-5 h-100">
         <MDBRow className="justify-content-center my-4">
@@ -30,107 +35,65 @@ import {
                 </MDBTypography>
               </MDBCardHeader>
               <MDBCardBody>
-                <MDBRow>
-                  <MDBCol lg="3" md="12" className="mb-4 mb-lg-0">
-                    <MDBRipple rippleTag="div" rippleColor="light"
-                      className="bg-image rounded hover-zoom hover-overlay">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
-                        className="w-100" />
-                      <a href="#!">
-                        <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" , }}>
-                        </div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-    
-                  <MDBCol lg="5" md="6" className=" mb-4 mb-lg-0">
-                    <p>
-                      <strong>Blue denim shirt</strong>
-                    </p>
-                    <p>Color: blue</p>
-                    <p>Size: M</p>
-    
-                    <MDBTooltip wrapperProps={{ size: "sm" }} wrapperClass="me-1 mb-2"
-                      title="Remove item">
-                      <MDBIcon fas icon="trash" />
-                    </MDBTooltip>
-    
-                    <MDBTooltip wrapperProps={{ size: "sm" , color: "danger" }} wrapperClass="me-1 mb-2"
-                      title="Move to the wish list">
-                      <MDBIcon fas icon="heart" />
-                    </MDBTooltip>
-                  </MDBCol>
-                  <MDBCol lg="4" md="6" className="mb-4 mb-lg-0">
-                    <div className="d-flex mb-4" style={{ maxWidth: "300px" }}>
-                      <MDBBtn className="px-3 me-2">
-                        <MDBIcon fas icon="minus" />
-                      </MDBBtn>
-    
-                      <MDBInput defaultValue={1} min={0} type="number" label="Quantity" />
-    
-                      <MDBBtn className="px-3 ms-2">
-                        <MDBIcon fas icon="plus" />
-                      </MDBBtn>
-                    </div>
-    
-                    <p className="text-start text-md-center">
-                      <strong>$17.99</strong>
-                    </p>
-                  </MDBCol>
-                </MDBRow>
+               { cart.map((data) => {
+                 <MDBRow>
+                 <MDBCol lg="3" md="12" className="mb-4 mb-lg-0">
+                   <MDBRipple rippleTag="div" rippleColor="light"
+                     className="bg-image rounded hover-zoom hover-overlay">
+                     <img
+                       src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
+                       className="w-100" />
+                     <a href="#!">
+                       <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" , }}>
+                       </div>
+                     </a>
+                   </MDBRipple>
+                 </MDBCol>
+   
+                 <MDBCol lg="5" md="6" className=" mb-4 mb-lg-0">
+                   <p>
+                     <strong>Blue denim shirt</strong>
+                   </p>
+                   <p>Color: blue</p>
+                   <p>Size: M</p>
+   
+                   <MDBTooltip wrapperProps={{ size: "sm" }} wrapperClass="me-1 mb-2"
+                     title="Remove item">
+                     <MDBIcon fas icon="trash" />
+                   </MDBTooltip>
+   
+                   <MDBTooltip wrapperProps={{ size: "sm" , color: "danger" }} wrapperClass="me-1 mb-2"
+                     title="Move to the wish list">
+                     <MDBIcon fas icon="heart" />
+                   </MDBTooltip>
+                 </MDBCol>
+                 <MDBCol lg="4" md="6" className="mb-4 mb-lg-0">
+                   <div className="d-flex mb-4" style={{ maxWidth: "300px" }}>
+                     <MDBBtn className="px-3 me-2">
+                       <MDBIcon fas icon="minus" />
+                     </MDBBtn>
+   
+                     <MDBInput defaultValue={1} min={0} type="number" label="Quantity" />
+   
+                     <MDBBtn className="px-3 ms-2">
+                       <MDBIcon fas icon="plus" />
+                     </MDBBtn>
+                   </div>
+   
+                   <p className="text-start text-md-center">
+                     <strong>$17.99</strong>
+                   </p>
+                 </MDBCol>
+               </MDBRow>
+
+               })
+                
+                }
     
                 <hr className="my-4" />
+                
     
-                <MDBRow>
-                  <MDBCol lg="3" md="12" className="mb-4 mb-lg-0">
-                    <MDBRipple rippleTag="div" rippleColor="light"
-                      className="bg-image rounded hover-zoom hover-overlay">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
-                        className="w-100" />
-                      <a href="#!">
-                        <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.2)" , }}>
-                        </div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-    
-                  <MDBCol lg="5" md="6" className=" mb-4 mb-lg-0">
-                    <p>
-                      <strong>Red hoodie</strong>
-                    </p>
-                    <p>Color: red</p>
-                    <p>Size: M</p>
-    
-                    <MDBTooltip wrapperProps={{ size: "sm" }} wrapperClass="me-1 mb-2"
-                      title="Remove item">
-                      <MDBIcon fas icon="trash" />
-                    </MDBTooltip>
-    
-                    <MDBTooltip wrapperProps={{ size: "sm" , color: "danger" }} wrapperClass="me-1 mb-2"
-                      title="Move to the wish list">
-                      <MDBIcon fas icon="heart" />
-                    </MDBTooltip>
-                  </MDBCol>
-                  <MDBCol lg="4" md="6" className="mb-4 mb-lg-0">
-                    <div className="d-flex mb-4" style={{ maxWidth: "300px" }}>
-                      <MDBBtn className="px-3 me-2">
-                        <MDBIcon fas icon="minus" />
-                      </MDBBtn>
-    
-                      <MDBInput defaultValue={1} min={0} type="number" label="Quantity" />
-    
-                      <MDBBtn className="px-3 ms-2">
-                        <MDBIcon fas icon="plus" />
-                      </MDBBtn>
-                    </div>
-    
-                    <p className="text-start text-md-center">
-                      <strong>$17.99</strong>
-                    </p>
-                  </MDBCol>
-                </MDBRow>
+               
               </MDBCardBody>
             </MDBCard>
     
