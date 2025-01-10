57f10dd0f9e8f997edb6877c14ccd7fc0b2eebc3
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2025-01-11 02:03:02 +0530

    cheat streak

--- src/Components/CartPage.jsx
+++ src/Components/CartPage.jsx
@@ -31,7 +31,7 @@ import { getCartTotal, removeItem, decreaseItemQuantity, increaseItemQuantity } 
         
         return (
         
-      //tired  
+      //tired  aah
     <section className="h-100 gradient-custom">
       <MDBContainer className="py-5 h-100">
         <MDBRow className="justify-content-center my-4">
